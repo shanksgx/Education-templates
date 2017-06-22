9d5d03a04aea7d7d0f522c8a71f758087193d839
shanks <526140067@qq.com>
2017-06-22 14:32:33 +0800

    新增加其他教育类模板
    新增加活动详情，教师队伍等模板

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,7 @@ Page({
     a2src: '../../image/a2.png',
     a3src: '../../image/a3.png',
     a4src: '../../image/a4.png',
+    signupimg:'../../image/signup.png',
     imgsrc:'https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=120&wh_rate=0&size=f648_364&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=1619440b1bc5580ece1e18c556a408d7&src=http%3A%2F%2Ft12.baidu.com%2Fit%2Fu%3D2318927072%2C2551034291%26fm%3D175%26s%3DF9E09B544C2154095A6098DF030090F1%26w%3D660%26h%3D370%26img.JPEG',
     iconsrc:'../../image/usercount.png',
     jtsrc:'../../image/icon-jt.png',
@@ -20,5 +21,22 @@ Page({
     interval: 5000,
     duration: 1000 
   },
-  
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '教育模板首页',
+      path: '/pages/index/index',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功")
+      },
+      fail: function (res) {
+        // 转发失败
+        onsole.log("转发失败")
+      }
+    }
+  }
 })
